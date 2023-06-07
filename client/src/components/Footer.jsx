72bfb1189f07cccfcc408a8489e5efd806a2a12e
Prakash Barui <99799194+prakashbarui2001@users.noreply.github.com>
2023-06-07 11:15:05 +0530

    Create Footer.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Write Your Blog and <b>Make It Big</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
